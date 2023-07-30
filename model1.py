6748e2319eb771d112f205b9a63c9186b22ae8ec
SidRakshit <sidrakshit5@gmail.com>
2023-07-30 17:40:58 +0530

    Broken into classes

--- model1.py
+++ model1.py
@@ -5,80 +5,89 @@ from sklearn.metrics import classification_report
 from sklearn.svm import SVC  
 from sklearn.ensemble import GradientBoostingClassifier
 
-# Usage:
-# fillna_with_group_means(df, 'Age', 'Sex')
-def fillna_with_group_means(df, fill_col, group_col):
-    # Calculate the group means
-    group_means = df.groupby(group_col)[fill_col].mean()
+class DataPrep:
 
-    # Function to apply
-    def replace_nan(row):
-        if pd.isnull(row[fill_col]):
-            return group_means[row[group_col]]
-        else:
-            return row[fill_col]
+    def __init__(self):
+        pass
 
-    # Apply the function
-    df[fill_col] = df.apply(replace_nan, axis=1)
+    def fillna_with_group_means(self, df, fill_col, group_col):
+        # Calculate the group means
+        group_means = df.groupby(group_col)[fill_col].mean()
 
-# Cleans the data and sets it up to be processed
-def clean_data(data, pred_inp):
-    fillna_with_group_means(data, "Age", "Sex")
-    fillna_with_group_means(pred_inp, "Age", "Sex")
-    data = pd.get_dummies(data)
-    pred_inp = pd.get_dummies(pred_inp)
-    return data, pred_inp
+        # Function to apply
+        def replace_nan(row):
+            if pd.isnull(row[fill_col]):
+                return group_means[row[group_col]]
+            else:
+                return row[fill_col]
 
-# Splits the data into train, validation and test subsets
-# Returns 6 data frames
-def split_data(features, target):
-    train_data = data[:int(0.5*len(data))]
-    val_data = data[int(0.5*len(data)):int(0.75*len(data))]
-    test_data = data[int(0.75*len(data)):]
-    return train_data[features], train_data[target], \
-        val_data[features], val_data[target], \
+        # Apply the function and create a new DataFrame
+        new_df = df.copy()
+        new_df[fill_col] = new_df.apply(replace_nan, axis=1)
+        return new_df
+
+    def clean_data(self, data, pred_inp):
+        data = self.fillna_with_group_means(data, "Age", "Sex")
+        pred_inp = self.fillna_with_group_means(pred_inp, "Age", "Sex")
+        return pd.get_dummies(data), pd.get_dummies(pred_inp)
+
+    def split_data(self, df, features, target):
+        train_data = df[:int(0.5*len(df))]
+        val_data = df[int(0.5*len(df)):int(0.75*len(df))]
+        test_data = df[int(0.75*len(df)):]
+        return (
+            train_data[features], train_data[target],
+            val_data[features], val_data[target],
             test_data[features], test_data[target]
+        )
 
-# Random Forest Classifier
-def RFC():
-    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-    model.fit(x_train, y_train)
-    return model
+class Model:
+    def __init__(self, x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+                        
+    # Random Forest Classifier
+    def RFC(self):
+        model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+        model.fit(self.x_train, self.y_train)
+        return model
 
-def LR():
-    logreg = LogisticRegression(random_state=16)
-    logreg.fit(x_train, y_train)
-    return logreg
+    # Logistic Regression
+    def LR(self):
+        logreg = LogisticRegression(random_state=16)
+        logreg.fit(self.x_train, self.y_train)
+        return logreg
 
-def SVM():
-    clf = SVC(kernel='linear')
-    clf.fit(x_train, y_train)
-    return clf
+    # Support Vector Machine
+    def SVM(self):
+        clf = SVC(kernel='linear')
+        clf.fit(self.x_train, self.y_train)
+        return clf
 
-def GBC():
-    gb_clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=42)
-    gb_clf.fit(x_train, y_train)
-    return gb_clf
+    # Gradient Boosting Classifier
+    def GBC(self):
+        gb_clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=42)
+        gb_clf.fit(self.x_train, self.y_train)
+        return gb_clf
 
 # Evaluates all the models
 def evaluate():
     # Random Forest Classification
-    rfc_report = classification_report(y_val, RFC().predict(x_val))
+    rfc_report = classification_report(y_val, model.RFC().predict(x_val))
     print("Random Forest Classification report: ")
     print(rfc_report)
     # Logistic Regression
-    lr_report = classification_report(y_val, LR().predict(x_val))
+    lr_report = classification_report(y_val, model.LR().predict(x_val))
     print("Logistic Regression report: ")
     print(lr_report)
     # Support Vector Machine
-    svm_report = classification_report(y_val, SVM().predict(x_val))
+    svm_report = classification_report(y_val, model.SVM().predict(x_val))
     print("Support Vector Machine report: ")
     print(svm_report)
     # Gradient Boosting Classic=fication
-    gbc_report = classification_report(y_val, GBC().predict(x_val))
+    gbc_report = classification_report(y_val, model.GBC().predict(x_val))
     print("Gradient Boosting Classification report: ")
     print(gbc_report)
-    
 
 # # Creates a csv for final submission
 # def create_csv():
@@ -88,11 +97,16 @@ def evaluate():
 
 data = pd.read_csv("train.csv")
 pred_inp = pd.read_csv("test.csv")
-data, pred_inp = clean_data(data, pred_inp)
-
 features = ["Pclass", "Age", "Sex_female", "Sex_male", "SibSp", "Parch"]
 target = ["Survived"]
-x_train, y_train, x_val, y_val, x_test, y_test = split_data(features, target)
 
+# Data Prep Stage
+dp = DataPrep()
+data, pred_inp = dp.clean_data(data, pred_inp)
+x_train, y_train, x_val, y_val, x_test, y_test = dp.split_data(data, features, target)
+
+# Modelling Stage
+model = Model(x_train, y_train)
+
+# Evaluation Stage
 evaluate()
-
